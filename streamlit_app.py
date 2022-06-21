e57b407e394f12d0174e9dc89430f38e0e3f553f
brickheadman <41268740+brickheadman@users.noreply.github.com>
2022-06-21 10:13:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & *Blueberry Oatmeal!* :sunglasses:')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilded Free-Range Egg')
+
